c10b718bd24452307d556d2b03ff6292b44ae68e
defendimattia <defendi.mattia92@gmail.com>
2025-06-25 17:56:44 +0200

    Update Studente class to include constructor for initializing fields

--- src/org/lessons/java/oop/snacks/Studente.java
+++ src/org/lessons/java/oop/snacks/Studente.java
@@ -4,4 +4,10 @@ public class Studente {
     private String firstname;
     private String lastname;
     private int age;
+
+    public Studente(String firstname, String lastname, int age) {
+        this.firstname = firstname;
+        this.lastname = lastname;
+        this.age = age;
+    }
 }
