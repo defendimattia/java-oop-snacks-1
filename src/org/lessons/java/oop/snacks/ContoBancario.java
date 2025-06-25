7b7c10c285b1f1dd226eb97bc3990ef6afcdcf37
defendimattia <defendi.mattia92@gmail.com>
2025-06-25 18:34:00 +0200

    Refactor ContoBancario class to include constructor for initializing bankAccount and balance

--- src/org/lessons/java/oop/snacks/ContoBancario.java
+++ src/org/lessons/java/oop/snacks/ContoBancario.java
@@ -5,6 +5,10 @@ import java.math.BigDecimal;
 public class ContoBancario {
     private String bankAccount;
     private BigDecimal balance;
-    
+
+    public ContoBancario(String bankAccount) {
+        this.bankAccount = bankAccount;
+        this.balance = BigDecimal.ZERO;
+    }
 
 }
