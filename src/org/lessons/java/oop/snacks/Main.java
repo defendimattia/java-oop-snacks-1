31822aa8ccc41dc0b236c814972fc68359b1eaa5
defendimattia <defendi.mattia92@gmail.com>
2025-06-25 17:52:39 +0200

    Add Main and Studente classes; remove App class

---
+++ src/org/lessons/java/oop/snacks/Main.java
@@ -0,0 +1,7 @@
+package org.lessons.java.oop.snacks;
+
+public class Main {
+public static void main(String[] args) {
+    
+}
+}
---
+++ src/org/lessons/java/oop/snacks/Studente.java
@@ -0,0 +1,7 @@
+package org.lessons.java.oop.snacks;
+
+public class Studente {
+    private String firstname;
+    private String lastname;
+    private int age;
+}
